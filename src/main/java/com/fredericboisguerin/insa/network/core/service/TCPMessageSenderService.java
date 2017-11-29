adbc9825a06c8c988d34896c020dcfc6a8339867
M. Poncy Jonathan <poncy@insa-08125>
2017-11-29 08:44:34 +0100

    TD4

--- src/main/java/com/fredericboisguerin/insa/network/core/service/TCPMessageSenderService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/TCPMessageSenderService.java
@@ -1,8 +1,16 @@
 package com.fredericboisguerin.insa.network.core.service;
 
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TCPMessageSenderService implements MessageSenderService {
     @Override
     public void sendMessageOn(String ipAddress, int port, String message) throws Exception {
-        throw new UnsupportedOperationException();
+        ServerSocket serverSocket = new ServerSocket(5000);
+        Socket chatSocket = serverSocket.accept();
+        PrintWriter writer = new PrintWriter(chatSocket.getOutputStream());
+        writer.println(message);
+        writer.close();
     }
 }
--- src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageReceiverService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageReceiverService.java
@@ -1,8 +1,18 @@
 package com.fredericboisguerin.insa.network.core.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class UDPMessageReceiverService implements MessageReceiverService {
     @Override
     public void listenOnPort(int port, IncomingMessageListener incomingMessageListener) throws Exception {
-        throw new UnsupportedOperationException();
+        incomingMessageListener.onNewIncomingMessage("Go");
+
+        Socket chatSocket = new Socket("127.0.0.1",port);
+        InputStreamReader stream = new InputStreamReader(chatSocket.getInputStream());
+        BufferedReader reader = new BufferedReader(stream);
+        String message = reader.readLine();
+        System.out.println("Server said: "+message);
     }
 }
